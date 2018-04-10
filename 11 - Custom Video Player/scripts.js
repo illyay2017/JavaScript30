46cf84a3685702e126e2b226e43b242e9d494cd9
illyay2017 <illya.yanchuk@gmail.com>
2018-04-10 14:10:29 -0700

    solved HTML5 videoPlayer

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,11 +1,12 @@
 /* SPECS:
 
-1) make play button toggle playback, and make click on the video player toggle playback
-2) make sliding the volume bar change the volume accordingly;
-3) make sliding the playback speed bar change the playback speed accordingly;
-4) make clicking the skip forward button skip forward 25s
-5) make clicking the skip back button skip back 10s
-6) make the progress bar reflect video progress
+DONE---> 1) make play button toggle playback, and make click on the video player toggle playback 
+DONE---> 2) make sliding the volume bar change the volume accordingly;
+DONE---> 3) make sliding the playback speed bar change the playback speed accordingly;
+DONE---> 4) make clicking the skip forward button skip forward 25s
+DONE---> 5) make clicking the skip back button skip back 10s
+6) make the progress bar reflect video progress, and clicking on it reflect to which 
+  point in the video we need to skip
 
 */
 
@@ -16,28 +17,14 @@ const video = document.querySelector('.viewer');
 // PLAYER BUTTONS:
 const playerButtons = document.querySelectorAll('.player__button');
 const toggle = playerButtons[0];
+const fullScreen = playerButtons[3];
 const skipButtons = document.querySelectorAll('[data-skip]');
 //PROGRESS BAR:
+const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 // SLIDERS:
 const sliders = document.querySelectorAll('.player__slider');
 
-// build our functions
-/*
-for spec 1:
- -- we need a togglePlay function 
-for spec 2:
- -- we need a changeVolume function
-for spec 3:
- -- we need a changePlaySpeed function
-for spec3:
- -- we need a skipForward function
-for spec5:
- -- we need a skipBack function
-for spec6:
- -- we need a updateProgress function???
-
-*/
 
 function togglePlay() {
   video.paused ? video.play() : video.pause();
@@ -55,16 +42,45 @@ function handleRangeUpdate() {
   video[this.name] = this.value;  
 }
 
+function updateprogress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
 
+function handleProgressBarClick(e) {
+  const progressTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = progressTime;
+}
+
+function changeFullScreenIcon(e) {
+  if (e.type === 'mouseover') {
+    this.textContent = '[ ] => [    ]';
+  } else if (e.type === 'mouseleave') {
+    this.textContent = '[ ]';
+  } else if (e.type === 'click') {
+    if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullScreen) {
+      video.webkitRequestFullScreen();
+    }
+  }
+}
+
+let mousedown = false;
 // hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updatePlayButton);
 video.addEventListener('pause', updatePlayButton);
+video.addEventListener('timeupdate', updateprogress);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 sliders.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
-
-
-
 toggle.addEventListener('click', togglePlay);
-// volume.addEventListener('click', changeVolume);
+fullScreen.addEventListener('mouseover', changeFullScreenIcon);
+fullScreen.addEventListener('mouseleave', changeFullScreenIcon);
+fullScreen.addEventListener('click', changeFullScreenIcon);
+
+progress.addEventListener('click', handleProgressBarClick);
+progress.addEventListener('mousemove', (e) => mousedown && handleProgressBarClick(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
